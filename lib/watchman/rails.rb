bfc895ceba2cd23177781198156a9f6cc1f3095d
Igor Šarčević <igor@renderedtext.com>
2017-05-04 17:25:00 +0200

    Bootstrap

---
+++ lib/watchman/rails.rb
@@ -0,0 +1,7 @@
+require "watchman/rails/version"
+
+module Watchman
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/watchman/rails/version.rb
@@ -0,0 +1,5 @@
+module Watchman
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
